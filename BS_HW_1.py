705916ab1ffaf9ccf58fcc71df113ee0e8032294
tdsimatupang <tdsimatupang@users.noreply.github.com>
2024-02-13 20:47:44 -0500

    I just commit my changes for Homework 1

--- BS_HW_1.py
+++ BS_HW_1.py
@@ -22,10 +22,36 @@ daily_shelter_proc.head()
 
 # In[5]:
 
-
-get_ipython().run_line_magic('matplotlib', 'inline')
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+import yaml
+
+parser = argparse.ArgumentParser(description='Daily shelter overnight occupancy')
+parser.add_argument('filename', type=str, help='Path to the configuration file')
+parser.add_argument('--plot_colors', '-pc', type=str, help='plot colors', default='green')
+args = parser.parse_args()
+
+job_config_paths = ['job_config.yml']
+job_config_paths += args.filename
+
+filename = {}
+for path in job_config_paths:
+    with open(args.filename, 'r') as f:
+        this_config = yaml.safe_load(f)
+        filename.update(this_config)
+
+filename = {"group_col" : "OCCUPIED_BEDS"}
+daily_shelter_proc.groupby(filename["group_col"])
+
+user_config = ['user_config.yml']
+user_config += args.plot_colors
+
+plot_colors = {}
+for color in plot_colors:
+    with open(args.plot_colors, 'r') as f:
+        this_user_config = yaml.safe_load(f)
+        plot_colors.update(this_user_config)
 
 occupied = plt.scatter(range(len(daily_shelter_proc)), daily_shelter_proc['OCCUPIED_BEDS'], label='Occupied Beds')
 unoccupied = plt.scatter(range(len(daily_shelter_proc)), daily_shelter_proc['UNOCCUPIED_BEDS'], label='Unoccupied Beds')
@@ -34,6 +60,12 @@ unoccupied = plt.scatter(range(len(daily_shelter_proc)), daily_shelter_proc['UNO
 plt.xlabel("Data Points")
 plt.ylabel("Number of Beds")
 plt.title("Occupied vs Unoccupied Beds")
+
+print(filename)
+
+print(args.filename)
+
+print(args.plot_colors)
 
 # Add a legend
 plt.legend()
